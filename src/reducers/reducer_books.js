43c616060f9204c7a61495dfb5cc95edc7e129ff
Johnny <johnny@ascendify.com>
2017-08-15 08:48:20 -0700

    [BookList] - Exercise Solutions

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,11 @@
 export default function() {
   return [
-    { title: 'Javascript: The Good Parts', pages: 101 },
-    { title: 'Harry Potter', pages: 39 },
-    { title: 'The Dark Tower', pages: 85 },
-    { title: 'Eloquent Ruby', pages: 1 }
+    { title: 'Javascript: The Good Parts', pages: 99 },
+    { title: 'A Song of Ice and Fire', pages: 750 },
+    { title: 'Return of the King', pages: 885 },
+    { title: 'Developing in React', pages: 50 },
+    { title: 'Eloquent Ruby', pages: 10 },
+    { title: 'Great Expectations', pages: 1000 },
+    { title: 'A Wrinkle In Time', pages: 300 }
   ];
 }
